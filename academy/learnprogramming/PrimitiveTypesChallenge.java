f2d3e52cdee38e870a995eafbe3a14ec7acac951
tylertamalunas <tylertamalunas@gmail.com>
2022-06-01 01:04:03 -0500

    added L to long values

--- academy/learnprogramming/PrimitiveTypesChallenge.java
+++ academy/learnprogramming/PrimitiveTypesChallenge.java
@@ -10,7 +10,7 @@ public class PrimitiveTypesChallenge {
         short firstShortValue = 10000;
         int firstIntValue = 2000000;
 
-        long firstLongValue = (50000 + (10 * (firstByteValue + firstShortValue + firstIntValue)));
+        long firstLongValue = (50000L + (10L * (firstByteValue + firstShortValue + firstIntValue)));
 
         System.out.println(firstLongValue);
     }
