cc6de3d572c67d84a530c198b74f9a1924316f82
tylertamalunas <tylertamalunas@gmail.com>
2022-06-01 03:59:02 -0500

    booleans and char variables

---
+++ academy/learnprogramming/CharandBoolean.java
@@ -0,0 +1,22 @@
+package academy.learnprogramming;
+
+public class CharandBoolean {
+
+    public static void main(String[] args) {
+
+        char myChar = 'D';
+//        char variable is similar to string, but can only store a single character.
+        char myUnicode = '\u00A9';
+        System.out.println(myUnicode);
+        System.out.println(myChar);
+
+        boolean myTrueValue = true;
+        boolean myFalseValue = false;
+        System.out.println(myTrueValue);
+        System.out.println(myFalseValue);
+
+        boolean isCustomerOverTwentyOne = true;
+
+    }
+
+}
