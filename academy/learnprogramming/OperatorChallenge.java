6f9101e246b7b81950bfe3c963c345d9029ed52d
tylertamalunas <tylertamalunas@gmail.com>
2022-06-02 03:15:37 -0500

    operator challenge

---
+++ academy/learnprogramming/OperatorChallenge.java
@@ -0,0 +1,18 @@
+package academy.learnprogramming;
+
+public class OperatorChallenge {
+
+    public static void main(String[] args) {
+
+        double firstDouble = 20.00d;
+        double secDouble = 80.00d;
+        double doubleTotal = ((firstDouble + secDouble) * 100.00d);
+        double doubleRemainder = doubleTotal % 40.00d;
+        boolean isZero = (doubleRemainder == 0) ? true : false;
+        System.out.println(isZero);
+
+        if (!isZero) {
+            System.out.println("Got some remainder.");
+        }
+    }
+}
