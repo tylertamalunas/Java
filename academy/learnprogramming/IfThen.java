3b464f112d7c6d3d0c65b8910f27aa811af97fc1
tylertamalunas <tylertamalunas@gmail.com>
2022-06-02 02:56:57 -0500

    ternary operator ?

--- academy/learnprogramming/IfThen.java
+++ academy/learnprogramming/IfThen.java
@@ -5,7 +5,7 @@ public class IfThen {
     public static void main(String[] args) {
 
         boolean isAlien = false;
-        if (isAlien == true) {
+        if (!isAlien) {
             System.out.println("It is not an Alien!");
             System.out.println("and Im scared of them.");
         }
@@ -20,6 +20,22 @@ public class IfThen {
 
         if ((topScore > 90) || (secTopScore <= 90)) {
             System.out.println("either or both of the conditions are true");
+        }
+
+        int newValue = 50;
+        if (newValue == 50) {
+            System.out.println("This is not an error");
+        }
+        boolean isCar = false;
+        if (isCar) {
+            System.out.println("thats not a car");
+        }
+
+        isCar = true;
+        boolean wasCar = isCar ? true : false;
+        if (wasCar) {
+            System.out.println("wasCar is true");
+
         }
     }
 }
