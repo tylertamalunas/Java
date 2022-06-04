a14c8a7f75dd7748d4345f5f61459349c97da8f9
tylertamalunas <tylertamalunas@gmail.com>
2022-06-04 17:21:56 -0500

    creating more methods that return different values

---
+++ academy/learnprogramming/HighScore.java
@@ -0,0 +1,37 @@
+package academy.learnprogramming;
+
+public class HighScore {
+
+    public static void main(String[] args) {
+        int lastScore = calculateHighScorePosition(1500);
+        displayHighScorePosition("Tyler", lastScore);
+
+        lastScore = calculateHighScorePosition(900);
+        displayHighScorePosition("Liz", lastScore);
+
+        lastScore = calculateHighScorePosition(400);
+        displayHighScorePosition("Magda", lastScore);
+
+        lastScore = calculateHighScorePosition(50);
+        displayHighScorePosition("Push", lastScore);
+
+    }
+
+    public static void displayHighScorePosition(String name, int position) {
+        System.out.println(name + " managed to get into position "
+                + position + " on the high score table.");
+
+    }
+
+    public static int calculateHighScorePosition(int score) {
+        if (score >= 1000) {
+            return 1;
+        } else if (score >= 500) {
+            return 2;
+        } else if (score >= 100) {
+            return 3;
+        } else {
+            return 4;
+        }
+    }
+}
