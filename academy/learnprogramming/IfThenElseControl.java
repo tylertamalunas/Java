c9d6a169122070e67211c1925c93f3c4153e762c
tylertamalunas <tylertamalunas@gmail.com>
2022-06-04 16:12:32 -0500

    creating methods

--- academy/learnprogramming/IfThenElseControl.java
+++ academy/learnprogramming/IfThenElseControl.java
@@ -3,22 +3,22 @@ package academy.learnprogramming;
 public class IfThenElseControl {
 
     public static void main(String[] args) {
-
         boolean gameOver = true;
         int score = 800;
         int levelCompleted = 5;
         int bonus = 100;
 
+        calculateScore(true, 800, 5, 100);
 
-        if (score < 5000) {
-            int finalScore = score + (levelCompleted * bonus);
-            System.out.println("Your final score was  " + finalScore);
+        calculateScore(true, 10000, 8, 200);
+
         }
-        score = 10000;
-        levelCompleted = levelCompleted + 8;
-        bonus = bonus +200;
+
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
+            finalScore += 2000;
             System.out.println("Your final score was " + finalScore);
         }
     }
