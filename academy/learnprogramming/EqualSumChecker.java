3faac655b57ba95fe2f3a79309c701f8b6f4b318
tylertamalunas <tylertamalunas@gmail.com>
2022-06-06 02:53:30 -0500

    equal sum checker

---
+++ academy/learnprogramming/EqualSumChecker.java
@@ -0,0 +1,9 @@
+package academy.learnprogramming;
+
+public class EqualSumChecker {
+
+    public static boolean hasEqualSum(int firstInt, int secInt, int thirdInt) {
+
+        return (firstInt + secInt == thirdInt);
+    }
+}
