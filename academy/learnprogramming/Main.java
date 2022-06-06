9f864c4d4bb5a8dcf42c808c5721cbf2d086b279
tylertamalunas <tylertamalunas@gmail.com>
2022-06-06 01:54:00 -0500

    MegabytesConverter challenge and separating code in Main method to test code without have other stuff printed.

--- academy/learnprogramming/Main.java
+++ academy/learnprogramming/Main.java
@@ -4,36 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int myValue = 10000;
-
-        int myMinIntValue = Integer.MIN_VALUE;
-        int myMaxIntValue = Integer.MAX_VALUE;
-        System.out.println("Integer Minimum Value = " + myMinIntValue);
-        System.out.println("Integer Maximum Value = " + myMaxIntValue);
-        System.out.println("Busted MAX Value = " + (myMaxIntValue + 1));
-        System.out.println("Busted MIN Value = " + (myMinIntValue - 1));
-
-        byte myMinByteValue = Byte.MIN_VALUE;
-        byte myMaxByteValue = Byte.MAX_VALUE;
-        System.out.println("Byte Minimum Value = " + myMinByteValue);
-        System.out.println("Byte Maximum Value = " + myMaxByteValue);
-
-        short myMinShortValue = Short.MIN_VALUE;
-        short myMaxShortValue = Short.MAX_VALUE;
-        System.out.println("Short Min Value " + myMinShortValue);
-        System.out.println("Short Max Value " + myMaxShortValue);
-
-        long myLongValue = 100L;
-        long myMinLongValue = Long.MIN_VALUE;
-        long myMaxLongValue = Long.MAX_VALUE;
-        System.out.println("Long Min Value " + myMinLongValue);
-        System.out.println("long Max Value " + myMaxLongValue);
-
-        int myTotal = (myMinIntValue / 2);
-
-        byte myNewByteValue = (byte) (myMinByteValue / 2);
-
-        short myNewShortValue = (short) (myMinShortValue / 2);
-
+        MegaBytesConverter.printMegaBytesAndKiloBytes(2500);
+        MegaBytesConverter.printMegaBytesAndKiloBytes(-1024);
+        MegaBytesConverter.printMegaBytesAndKiloBytes(5000);
     }
 }
---
+++ academy/learnprogramming/MegaBytesConverter.java
@@ -0,0 +1,15 @@
+package academy.learnprogramming;
+
+public class MegaBytesConverter {
+
+    public static void printMegaBytesAndKiloBytes(int kiloBytes){
+        // 1 MB = 1024 KB
+        if (kiloBytes < 0) {
+            System.out.println("Invalid Value");
+        }else {
+            int megabytes = kiloBytes / 1024;
+            int leftoverkilobytes = kiloBytes % 1024;
+            System.out.println(kiloBytes + " KB = " + megabytes + " MB and " + leftoverkilobytes + " KB");
+        }
+    }
+}
