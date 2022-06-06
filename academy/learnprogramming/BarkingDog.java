9e51e74e0d5e3dec794fe09b6b424e780a2080ff
tylertamalunas <tylertamalunas@gmail.com>
2022-06-06 02:22:03 -0500

    LeapYear calculator

---
+++ academy/learnprogramming/BarkingDog.java
@@ -0,0 +1,15 @@
+package academy.learnprogramming;
+
+public class BarkingDog {
+
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+
+        if (hourOfDay < 0 || hourOfDay > 23) {
+            return false;
+        } else if ((barking && hourOfDay < 8) || (barking && hourOfDay > 22)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
