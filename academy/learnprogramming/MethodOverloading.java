0bee3ef5ba6d7e1ea382b1f6affde89c48ea1051
tylertamalunas <tylertamalunas@gmail.com>
2022-06-07 00:52:29 -0500

    learnign method overloading

--- academy/learnprogramming/MethodOverloading.java
+++ academy/learnprogramming/MethodOverloading.java
@@ -2,25 +2,20 @@ package academy.learnprogramming;
 
 public class MethodOverloading {
 
-    public static void main(String[] args) {
-        int newScore = calulateScore("Tyler", 500);
-        System.out.println("New score is " + newScore);
-        calulateScore(75);
-        calulateScore();
+    public static double calcFeetAndInchesToCentimeters(double feet, double inches) {
 
-    }
-    public static int calulateScore(String playerName, int score) {
-        System.out.println("Player " + playerName + " scored " + score + " points");
-        return score * 1000;
-    }
+        if (feet >= 0 && inches >=0) {
+            double feetToInches = feet * 12;
+            inches = feetToInches + inches;
+            return inches * 2.54;
+        }
+        return -1;
+        }
+    public static double calcFeetAndInchesToCentimeters(double inches) {
 
-    public static int calulateScore(int score) {
-        System.out.println("Unnamed player scored " + score + " points");
-        return score * 1000;
+        if (inches >= 0) {
+            return inches / 12;
+        }
+        return -1;
     }
-    public static int calulateScore() {
-        System.out.println("No player name, no score.");
-        return 0;
-    }
-
 }
