11bdad86fcbcf1f7deb1087888af336f97be557a
tylertamalunas <tylertamalunas@gmail.com>
2022-06-07 03:03:41 -0500

    practice challenges

--- academy/learnprogramming/Main.java
+++ academy/learnprogramming/Main.java
@@ -4,8 +4,5 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(SecondsAndMinutes.getDurationString(65, 45));
-        System.out.println(SecondsAndMinutes.getDurationString(3945));
-
     }
 }
