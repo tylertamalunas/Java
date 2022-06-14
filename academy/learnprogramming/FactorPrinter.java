5efd0ea59845f1189863c3c55b2919121157f2f3
tylertamalunas <tylertamalunas@gmail.com>
2022-06-14 03:26:42 -0500

    Coding exercises, parsing, user input

---
+++ academy/learnprogramming/FactorPrinter.java
@@ -0,0 +1,22 @@
+package academy.learnprogramming;
+
+public class FactorPrinter {
+    public static void main(String[] args) {
+printFactors(-1);
+    }
+
+    public static void printFactors(int number) {
+
+        if (number < 1) {
+            System.out.println("Invalid Value");
+        }
+        int factor = 1;
+        while (factor <= number) {
+            if (number % factor == 0) {
+                String x = String.valueOf(factor);
+                System.out.println(x);
+            }
+            factor++;
+        }
+    }
+}
---
+++ academy/learnprogramming/ParsingValues.java
@@ -0,0 +1,17 @@
+package academy.learnprogramming;
+
+public class ParsingValues {
+
+    public static void main(String[] args) {
+        String numberAsString = "2018.125";
+        System.out.println("numberAsString = " + numberAsString);
+
+        double number = Double.parseDouble(numberAsString);
+        System.out.println(number);
+
+        numberAsString += 1;
+        number += 1;
+        System.out.println(number);
+        System.out.println(numberAsString);
+    }
+}
