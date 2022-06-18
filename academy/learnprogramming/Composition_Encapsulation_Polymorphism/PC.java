7424642accce57c26e5d9d06feb24eaf36fafc61
tylertamalunas <tylertamalunas@gmail.com>
2022-06-17 21:41:18 -0500

    more inheritance, classes, constructors. Starting learning about Composition.

---
+++ academy/learnprogramming/Composition_Encapsulation_Polymorphism/PC.java
@@ -0,0 +1,26 @@
+package academy.learnprogramming.Composition_Encapsulation_Polymorphism;
+
+public class PC {
+
+    private Case theCase; // cant use "case" because that is reserved keyword
+    private Monitor monitor;
+    private Motherboard motherboard;
+
+    public PC(Case theCase, Monitor monitor, Motherboard motherboard) {
+        this.theCase = theCase;
+        this.monitor = monitor;
+        this.motherboard = motherboard;
+    }
+
+    public Case getTheCase() {
+        return theCase;
+    }
+
+    public Monitor getMonitor() {
+        return monitor;
+    }
+
+    public Motherboard getMotherboard() {
+        return motherboard;
+    }
+}
---
+++ academy/learnprogramming/Inheritance/Car.java
@@ -0,0 +1,35 @@
+package academy.learnprogramming.Inheritance;
+
+public class Car extends Vehicle{
+
+    private int wheels;
+    private int doors;
+    private int gear;
+    private boolean isManual;
+
+    private int currentGear;
+
+    public Car(String name, String size, int wheels, int doors, int gear, boolean isManual) {
+        super(name, size);
+        this.wheels = wheels;
+        this.doors = doors;
+        this.gear = gear;
+        this.isManual = isManual;
+        this.currentGear = 1;
+    }
+
+    public void changeGear(int currentGear) {
+        this.currentGear = currentGear;
+        System.out.println("Car.setCurrentGear(): Changed to " + this.currentGear + " gear.");
+    }
+
+    public void changeVelocity(int speed, int direction) {
+        move(speed, direction);
+        System.out.println("Car.changeVelocity(): Velocity " + speed + " direction " + direction);
+    }
+
+//    @Override
+//    public void stop() {
+//        super.stop();
+//    }
+}
