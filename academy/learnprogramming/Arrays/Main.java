f967decbb9b0b9dca456d1e54d4ca117af2eba32
tylertamalunas <tylertamalunas@gmail.com>
2022-06-21 03:31:41 -0500

    starting arrays

---
+++ academy/learnprogramming/Arrays/Main.java
@@ -0,0 +1,11 @@
+package academy.learnprogramming.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] myIntArray = new int[10];
+        myIntArray[5] = 50;
+
+        System.out.println(myIntArray[5]);
+    }
+}
