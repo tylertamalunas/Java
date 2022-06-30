5cb1651750f882b3b3ca48a64774080dc8b12d93
tylertamalunas <tylertamalunas@gmail.com>
2022-06-30 01:58:04 -0500

    mobile phone arraylist challenge

---
+++ academy/learnprogramming/ArrayLists/MobilePhone.java
@@ -0,0 +1,73 @@
+package academy.learnprogramming.ArrayLists;
+
+import java.util.ArrayList;
+
+public class MobilePhone {
+    private String myNumber;
+    private ArrayList<Contact> myContacts;
+
+    public MobilePhone(String myNumber) {
+        this.myNumber = myNumber;
+        this.myContacts = new ArrayList<Contact>();
+    }
+
+    public boolean addNewContact(Contact newContact) {
+        if (myContacts.contains(newContact)) {
+            System.out.println("Contact already in list.");
+            return false;
+        }
+        myContacts.add(newContact);
+        return true;
+    }
+
+    public boolean updateContact(Contact oldContact, Contact newContact) {
+        int contactExists = findContact(oldContact);
+        if (!myContacts.contains(oldContact)) {
+            System.out.println(oldContact + " not found");
+            return false;
+        }
+        this.myContacts.set(contactExists, newContact);
+        System.out.println(oldContact + " replaced with " + newContact);
+        return true;
+    }
+
+    public boolean removeContact(Contact oldContact) {
+        if (myContacts.contains(oldContact)) {
+            myContacts.remove(oldContact);
+            System.out.println(oldContact + " was removed");
+            return true;
+        }
+        System.out.println(oldContact + " not found");
+        return false;
+    }
+
+    private int findContact(Contact contact) {
+        return myContacts.indexOf(contact);
+    }
+
+    private int findContact(String name) {
+        for (int i = 0; i < this.myContacts.size(); i++) {
+            Contact contact = this.myContacts.get(i);
+            if (contact.getName().equals(name)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public String queryContact(Contact name) {
+        if (myContacts.contains(name)) {
+            return name.getName();
+        } else {
+            return null;
+        }
+    }
+
+    public void printContact() {
+        for (int i = 0; i < this.myContacts.size(); i++) {
+            System.out.println((i+1) +
+                    this.myContacts.get(i).getName() + " -> " +
+                    this.myContacts.get(i).getPhoneNumber());
+        }
+    }
+}
